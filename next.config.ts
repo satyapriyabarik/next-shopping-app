a5b9413889663e2971fcb79619a96c72eed4d086
Satyapriya Barik <satyapriya.barik@nagarro.com>
2025-11-01 11:13:41 +0530

    path to enable

--- next.config.ts
+++ next.config.ts
@@ -30,22 +30,27 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "encrypted-tbn0.gstatic.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "media.licdn.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "encrypted-tbn3.gstatic.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "treemart.com",
+        pathname: "/**",
       },
     ],
   },
